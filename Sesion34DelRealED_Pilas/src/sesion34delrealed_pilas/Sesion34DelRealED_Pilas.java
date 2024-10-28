2c32c3a0d0f0c8713fba9ad0229e7a11bd271c8a
DRSJir <jairdelreal@gmail.com>
2024-10-28 15:36:15 -0600

    Confirmación Del Real
    Datos desarrollo: Del real

---
+++ Sesion34DelRealED_Pilas/src/sesion34delrealed_pilas/Sesion34DelRealED_Pilas.java
@@ -0,0 +1,20 @@
+package sesion34delrealed_pilas;
+
+import java.util.Stack;
+import javax.swing.JOptionPane;
+
+public class Sesion34DelRealED_Pilas {
+
+    public static void main(String[] args) {
+        //Declaracion de pila
+        Stack<Integer> pilaDelReal = new Stack<>();
+        pilaDelReal.push(1);
+        pilaDelReal.push(2);
+        pilaDelReal.push(3);
+        pilaDelReal.push(4);
+        
+        JOptionPane.showMessageDialog(null, "Datos en pila ordenada es: \n" +pilaDelReal);
+        
+    }
+    
+}
